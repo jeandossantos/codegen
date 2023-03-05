e10e64cc55b9c5b4df242ce20f2f27db2e167e39
Jean F. dos Santos <jeanddg@hotmail.com>
2023-03-05 13:05:10 -0300

    feat: add createLayerIfNotExists()

---
+++ src/createLayer.js
@@ -0,0 +1,18 @@
+import fsPromise from 'node:fs/promises';
+import fs from 'node:fs';
+
+export async function createLayerIfNotExists({ mainPath, defaultPath, layer }) {
+  const path = `${defaultPath}/${mainPath}`;
+
+  const layerExists = fs.existsSync(`${path}/${layer}`);
+
+  let pendingPromise = undefined;
+
+  if (!layerExists) {
+    pendingPromise = fsPromise.mkdir(`${path}/${layer}`, {
+      recursive: true,
+    });
+  }
+
+  return Promise.all([pendingPromise]);
+}
