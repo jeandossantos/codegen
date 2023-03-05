00c7a73fda1df08a5644e0605406422ea05b83ff
Jean F. dos Santos <jeanddg@hotmail.com>
2023-03-05 14:28:58 -0300

    feat: export javascript templates

---
+++ src/templates/js/index.js
@@ -0,0 +1,11 @@
+import { generateRepositoryTemplate } from './repositoryTemplate.js';
+import { generateServiceTemplate } from './serviceTemplate.js';
+import { generateControllerTemplate } from './controllerTemplate.js';
+import { generateFactoryTemplate } from './factoryTemplate.js';
+
+export {
+  generateRepositoryTemplate,
+  generateServiceTemplate,
+  generateControllerTemplate,
+  generateFactoryTemplate,
+};
