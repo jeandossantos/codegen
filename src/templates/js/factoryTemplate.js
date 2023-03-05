01a1fe4a254f4410c74d810cc5b40c3bf9f705ab
Jean F. dos Santos <jeanddg@hotmail.com>
2023-03-05 11:03:09 -0300

    feat: add generateFactoryTemplate

---
+++ src/templates/js/factoryTemplate.js
@@ -0,0 +1,70 @@
+import { Util } from '../../util.js';
+
+const componentNameAnchor = '$$componentName';
+const repositoryNameAnchor = '$$repositoryName';
+const serviceNameAnchor = '$$serviceName';
+const controllerNameAnchor = '$$controllerName';
+
+const repositoryFilenameAnchor = '$$repositoryFilename';
+const serviceFilenameAnchor = '$$serviceFilename';
+const controllerFilenameAnchor = '$$controllerFilename';
+
+const repositoryInstanceAnchor = '$$repositoryInstance';
+const serviceInstanceAnchor = '$$serviceInstance';
+
+const template = `import $$repositoryNameRepository from './$$repositoryFilenameRepository.js';
+import $$serviceNameService from './$$serviceFilenameService.js';
+import $$controllerNameController from './productController.js';
+
+export default class $$componentNameFactory {
+  static getInstance() {
+    const $$repositoryInstanceRepository = new $$repositoryNameRepository();
+
+    const $$serviceInstanceService = new $$serviceNameService({
+      $$repositoryInstanceRepository,
+    });
+
+    const controller = new $$controllerNameController({
+      $$serviceInstanceService,
+    });
+
+    return controller;
+  }
+}`;
+
+export function generateFactoryTemplate(componentName) {
+  return {
+    filename: `${Util.lowercaseFirstLetter(componentName)}Factory`,
+    template: template
+      .replaceAll(componentNameAnchor, Util.uppercaseFirstLetter(componentName))
+      .replaceAll(
+        repositoryNameAnchor,
+        Util.uppercaseFirstLetter(componentName)
+      )
+      .replaceAll(serviceNameAnchor, Util.uppercaseFirstLetter(componentName))
+      .replaceAll(
+        controllerNameAnchor,
+        Util.uppercaseFirstLetter(componentName)
+      )
+      .replaceAll(
+        repositoryFilenameAnchor,
+        Util.lowercaseFirstLetter(componentName)
+      )
+      .replaceAll(
+        serviceFilenameAnchor,
+        Util.lowercaseFirstLetter(componentName)
+      )
+      .replaceAll(
+        controllerFilenameAnchor,
+        Util.lowercaseFirstLetter(componentName)
+      )
+      .replaceAll(
+        repositoryInstanceAnchor,
+        Util.lowercaseFirstLetter(componentName)
+      )
+      .replaceAll(
+        serviceInstanceAnchor,
+        Util.lowercaseFirstLetter(componentName)
+      ),
+  };
+}
